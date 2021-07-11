94437720dad1dee75626fa3a5b3810cae727cae7
Jose Llanes <josega88@gmail.com>
2021-07-11 02:39:01 -0500

    fixed route

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "MCSS", path: "./Sources/MCSS/MCSS.xcframework")
+        .binaryTarget(name: "MCSS", path: "./Sources/MCSS.xcframework")
       
     ]
     
