b494085294d5a6be3335bc51f49a2f635c512080
Jose Llanes <josega88@gmail.com>
2021-08-21 16:18:55 -0500

    Adding new  framework version

---
+++ Sources/MCSSLib.xcframework/ios-arm64/MCSSLIB.framework/Headers/MCSSLIB.h
@@ -0,0 +1,18 @@
+//
+//  MCSSLIB.h
+//  MCSSLIB
+//
+//  Created by Jose Llanes on 17/07/21.
+//  Copyright © 2021, Do Genius On, LLC. All Rights Reserved.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MCSSLIB.
+FOUNDATION_EXPORT double MCSSLIBVersionNumber;
+
+//! Project version string for MCSSLIB.
+FOUNDATION_EXPORT const unsigned char MCSSLIBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCSSLIB/PublicHeader.h>
+
+
